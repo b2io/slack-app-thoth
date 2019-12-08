26ac451b111d69d175bdc4b4bc69a57372e980f3
Drew Miller <dmiller@base2.io>
2019-12-08 13:40:35 -0500

    TODO add slash command (#7)
    - Add DynamoDB table for TODOs
    - Prefer us-east-2 (Ohio) region for deployment
    - Run smart-add on command text for the todo::add sub-command
    
    Closes #2

--- src/thoth.js
+++ src/thoth.js
@@ -1,27 +1,80 @@
 const { App, ExpressReceiver, LogLevel } = require('@slack/bolt');
 const { createServer, proxy } = require('aws-serverless-express');
 
+const smartAdd = require('./smart-add');
+const todos = require('./todos');
+
 const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 const app = new App({
-  logLevel: LogLevel.DEBUG,
+  logLevel: process.env.IS_OFFLINE ? LogLevel.DEBUG : LogLevel.WARN,
   receiver,
   token: process.env.SLACK_BOT_TOKEN,
 });
 
 app.command('/todo', async ({ ack, command, respond }) => {
   ack();
-  respond({
-    blocks: [
-      {
-        text: { text: `\`/todo ${command.text}\``, type: 'mrkdwn' },
-        type: 'section',
-      },
-    ],
-    response_type: 'ephemeral',
-  });
+  if (command.text.startsWith('add ')) {
+    try {
+      const input = command.text.replace(/^add /, '');
+      const todo = smartAdd(input);
+
+      const id = await todos.create({ ...todo, createdBy: command.user_id });
+
+      respond({
+        response_type: 'ephemeral',
+        text: `:heavy_check_mark: Created TODO "${input}"`,
+      });
+    } catch (error) {
+      respond({
+        response_type: 'ephemeral',
+        text: `:rotating_light: ${error}`,
+      });
+    }
+  } else {
+    respond({
+      blocks: [
+        {
+          text: {
+            text: `:wave: Need some help with \`/todo\`?`,
+            type: 'mrkdwn',
+          },
+          type: 'section',
+        },
+        {
+          text: {
+            text: `Use \`/todo\` to keep track of your TODOs. Some examples include:`,
+            type: 'mrkdwn',
+          },
+          type: 'section',
+        },
+        {
+          text: {
+            text: `- \`/todo add Pick up the milk\``,
+            type: 'mrkdwn',
+          },
+          type: 'section',
+        },
+        {
+          text: {
+            text: `- \`/todo add Stand-up meeting +ProjectName\``,
+            type: 'mrkdwn',
+          },
+          type: 'section',
+        },
+        {
+          text: {
+            text: `- \`/todo add Emergency bug fix +ProjectName %in-progress\``,
+            type: 'mrkdwn',
+          },
+          type: 'section',
+        },
+      ],
+      response_type: 'ephemeral',
+    });
+  }
 });
 
 app.error(error => {
